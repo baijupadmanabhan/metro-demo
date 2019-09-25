069dc8a9efe68a6b7a9d01f0c92daf6af11520d3
Baiju Padmanabhan <baiju_padmanabhan@intuit.com>
2019-09-25 23:36:35 +0530

    Fixing path export issue

--- terraform/modules/ec2/install-nginx.sh
+++ terraform/modules/ec2/install-nginx.sh
@@ -14,4 +14,4 @@ git clone https://github.com/baijupadmanabhan/metro-demo.git
 cd metro-demo/ansible/
 ansible-playbook playbook.yml
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone -s | sed 's/[a-z]$//')
-aws ec2 create-tags --resources $(curl http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=Bastion --region ${REGION}
+aws ec2 create-tags --resources $(curl http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=Webserver --region ${REGION}
