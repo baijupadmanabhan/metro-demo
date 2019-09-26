ba38e63fe906eb28479dcd2b9a0c8a0760d03f11
Baiju Padmanabhan <baiju_padmanabhan@intuit.com>
2019-09-26 08:49:23 +0530

    Updating userdata

--- terraform/modules/ec2/install-nginx.sh
+++ terraform/modules/ec2/install-nginx.sh
@@ -13,5 +13,9 @@ pip install --upgrade ansible
 git clone https://github.com/baijupadmanabhan/metro-demo.git
 cd metro-demo/ansible/
 ansible-playbook playbook.yml
+source ~/.bashrc
+go get -v -u github.com/gorilla/mux
+cd /root/go-project; go install main.go
+nohup main >/dev/null 2>&1 &
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone -s | sed 's/[a-z]$//')
 aws ec2 create-tags --resources $(curl http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=Webserver --region ${REGION}
