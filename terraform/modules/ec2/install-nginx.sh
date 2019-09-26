dbbd570cfcde54fed6911397a28a789651b745a9
Baiju Padmanabhan <baiju_padmanabhan@intuit.com>
2019-09-26 13:27:30 +0530

    Adding docker container for go

--- terraform/modules/ec2/install-nginx.sh
+++ terraform/modules/ec2/install-nginx.sh
@@ -14,21 +14,29 @@ git clone https://github.com/baijupadmanabhan/metro-demo.git
 cd metro-demo/ansible/
 ansible-playbook playbook.yml
 
-cd /usr/local/
-sudo wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
-tar -xzf go1.13.linux-amd64.tar.gz
-echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bash_profile
-source /root/.bash_profile
-mkdir ~/go-workspace
-cp /metro-demo/Golang/main.go ~/go-workspace/
-cp /metro-demo/Golang/main ~/go-workspace/
-export GOROOT=/usr/local/go
-export GOPATH=~/go-workspace
-export GOBIN="$GOPATH/bin"
-export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+# cd /usr/local/
+# sudo wget https://dl.google.com/go/go1.13.linux-amd64.tar.gz
+# tar -xzf go1.13.linux-amd64.tar.gz
+# echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bash_profile
+# source /root/.bash_profile
+# mkdir ~/go-workspace
+# cp /metro-demo/Golang/main.go ~/go-workspace/
+# cp /metro-demo/Golang/main ~/go-workspace/
+# export GOROOT=/usr/local/go
+# export GOPATH=~/go-workspace
+# export GOBIN="$GOPATH/bin"
+# export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
  
-go get -v -u github.com/gorilla/mux &&
-# cd /root/go-workspace; go install main.go
-nohup /metro-demo/Golang/main >/dev/null 2>&1 &
+# go get -v -u github.com/gorilla/mux
+# # cd /root/go-workspace; go install main.go
+# echo "Starting application"
+# nohup /metro-demo/Golang/main >/dev/null 2>&1 &
+
+sudo amazon-linux-extras install docker
+sudo service docker start
+cd /metro-demo/Golang/
+docker build -t TokenGenerator .
+docker run -t -i -p 8080:8080
+
 REGION=$(curl http://169.254.169.254/latest/meta-data/placement/availability-zone -s | sed 's/[a-z]$//')
 aws ec2 create-tags --resources $(curl http://169.254.169.254/latest/meta-data/instance-id) --tags Key=Name,Value=Webserver --region ${REGION}
